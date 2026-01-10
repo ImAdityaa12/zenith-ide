1f58aaa40ff5456706306f4fdf4e0a22861d8a7f
ImAdityaa12 <adityagupta1291@gmail.com>
2026-01-10 19:30:06 +0530

    Minor Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${plexMono.variable} antialiased`}
